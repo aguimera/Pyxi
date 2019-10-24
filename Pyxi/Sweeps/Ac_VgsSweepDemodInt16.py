cb37b4d05639386dc9c03654c913052beddc3474
luciare <lucia.re@imb-cnm.csic.es>
2019-10-24 16:31:10 +0200

    DemodWithSlope

--- Pyxi/Sweeps/Ac_VgsSweepDemodInt16.py
+++ Pyxi/Sweeps/Ac_VgsSweepDemodInt16.py
@@ -8,6 +8,7 @@ Created on Mon Jul 29 13:09:18 2019
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
+from scipy.stats import linregress as lnr
 
 import os
 import h5py
@@ -36,7 +37,7 @@ if __name__ == '__main__':
 #=======
 #    Dictname=r"C:\Users\Lucia\Dropbox (ICN2 AEMD - GAB GBIO)\PyFET\LuciaScripts\Lucia\DCTests\Transistor\19_09_2019\SSP54348-T2-3x3-Sig10mVp10Hz"
 #>>>>>>> f76cb064240fab7757d563ad181ce0ea254c7eb8
-    Dictname =r"F:\Dropbox (ICN2 AEMD - GAB GBIO)\TeamFolderLMU\FreqMux\Lucia\DCTests\Transistors\24_10_2019\TestS4_NoDCFilt_t30_stab20"
+    Dictname =r"C:\Users\Lucia\Dropbox (ICN2 AEMD - GAB GBIO)\TeamFolderLMU\FreqMux\Lucia\DCTests\Transistors\24_10_2019\TestS4_NoDCFilt_t30_stab20"
 
     FileName = Dictname +'_0'+'.h5'
     hfile = h5py.File(FileName, 'r')
@@ -80,26 +81,14 @@ if __name__ == '__main__':
                 axPsd.plot(ff[pi], psdadem[pi], 'k*')
             axTemp.plot(Iin[:int(DemArgs['Samps'])*2], label=Lab)   
         
-#%%      
-#    fig, axTemp = plt.subplots()
-#    for dem, DemArgs in ProcsDict.items():
-#        Lab = str(DemArgs['dset']) +'-'+ str(DemArgs['dInd'])
-#        if FloatType:
-#                LSB = 1
-#        else:
-#                LSB = DemArgs['LSB'][DemArgs['dInd']]
-#                
-#        Iin = ((data[DemArgs['dset']][:, DemArgs['dInd']])*LSB)/DemArgs['Gain']
-#        axTemp.plot(Iin, label=Lab) 
+
 #%%       
     Procs = []
     Labs = []
     AcqArgs = []
-#<<<<<<< HEAD
-#    DivProcs = 4 
-#=======
+
     DivProcs = 7
-#>>>>>>> f76cb064240fab7757d563ad181ce0ea254c7eb8
+
     results = []
     for dem, DemArgs in ProcsDict.items():
 #        if DemArgs['dInd'] != 1:
@@ -113,8 +102,6 @@ if __name__ == '__main__':
         else:
                 LSB = DemArgs['LSB'][DemArgs['dInd']]
         Iin = ((data[DemArgs['dset']][:, DemArgs['dInd']])*LSB)/DemArgs['Gain']
-#        Iin = ((data[DemArgs['dset']][:, 0])*LSB)/DemArgs['Gain']
-#        Iin = ((data[DemArgs['dset']][:, DemArgs['dInd']])*DemArgs['LSB'][DemArgs['dInd']])/DemArgs['Gain']
 
         Lab = str(DemArgs['dset']) +'-'+ str(DemArgs['ProbeRow']+str(DemArgs['col']))
         print(Lab)     
@@ -146,7 +133,7 @@ if __name__ == '__main__':
         
   
 #%%
-    DelaySamps = 200
+    DelaySamps = 0
     fig, axres = plt.subplots()  
     axres.set_title('Vgs')
     fig, axR = plt.subplots()  
@@ -174,20 +161,23 @@ if __name__ == '__main__':
         ptrend = np.polyfit(x, adems, 1)
         trend = np.polyval(ptrend, x)
         ACarr = (2*ptrend[1])/np.sqrt(2)
-
+        slope = lnr(x, trend)
+        
+        while slope > 1e-14:
+            DelaySamps = DelaySamps + 50
+            adems = np.abs(dem[DelaySamps:])
+            x = np.arange(adems.size)
+            ptrend = np.polyfit(x, adems, 1)
+            trend = np.polyval(ptrend, x)
+            ACarr = (2*ptrend[1])/np.sqrt(2)
+            slope = lnr(x, trend)
+         
+        DelaySamps = 0
         R= (acqargs['Ac']/np.sqrt(2))/ACarr
         axR.plot(acqargs[xAxispar], R, '*')
         OutDict[TName+'Ac'+str((lab.split('Sw'))[1])] = np.append(OutDict[TName +'Ac'+str((lab.split('Sw'))[1])], ACarr)
-<<<<<<< HEAD
-#        if lab.split('Sw')[2][0:3] == '002':
-#            plt.figure(20)
-##            time = np.arange(0,len(adems)*1.0/FsOut,1.0/FsOut)
-#            ff, PSD = signal.welch(adems,FsOut,scaling='density',nperseg=2**13)
-#            plt.loglog(ff,PSD)
-=======
-        
->>>>>>> 4ccd6c2b7c62d7c2c9b869cf04052218f25c4e5d
-        DataDict[lab] = np.append(DataDict[lab], (adems-trend)) #no tiene en cuenta Vgs sweep CACA
+
+        DataDict[lab] = np.append(DataDict[lab], (adems-trend)) 
 
         axres.plot(acqargs[xAxispar], ACarr, '*')
     axres.set_xlabel(xAxispar)
