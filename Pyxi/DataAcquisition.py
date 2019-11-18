6163e6cf2d26e11fcaa8d1fa307042445d6a95f5
luciare <lucia.re@imb-cnm.csic.es>
2019-11-18 15:21:07 +0100

    FreqDaq

--- Pyxi/DataAcquisition.py
+++ Pyxi/DataAcquisition.py
@@ -10,162 +10,36 @@ import pyqtgraph.parametertree.parameterTypes as pTypes
 import pyqtgraph.parametertree.Parameter as pParams
 
 import numpy as np
-import niscope
-import nifgen
+import copy
+import time
+import re
+import numpy as np
 import copy
 import time
 import re
 
-import Pyxi.NifGenerator as NifGen
-import Pyxi.NiScope as NiScope
+import FMAcqCore as CoreMod
 
+#DAQ Data Acq
 class DataAcquisitionThread(Qt.QThread):
-    NewData = Qt.pyqtSignal()
+    NewMuxData = Qt.pyqtSignal()
 
-    def __init__(self, ColsConfig, FsGen, GenSize, CMVoltage, RowsConfig, FsScope, BufferSize, NRow, OffsetRows, GainBoard, ResourceScope):
-        print ('TMacqThread, DataAcqThread')
+    def __init__(self, ChannelsConfigKW, SampKw, AvgIndex=5):
         super(DataAcquisitionThread, self).__init__()
-        
-        print(ColsConfig)
-        print(RowsConfig)
-        self.Cols = NifGen.Columns(ColsConfig=ColsConfig, 
-                                   FsGen=FsGen, 
-                                   GenSize=GenSize)
-        self.Rows = NiScope.Rows(RowsConfig=RowsConfig, 
-                                 FsScope=FsScope, 
-                                 ResourceScope=ResourceScope)
-        self.BufferSize = BufferSize
-        self.OffsetRows = OffsetRows
-        self.GainBoard = (GainBoard)
-        self.LSB = np.array([])
-        self.RowsList = []
-        self.Channels = []
-        
-        self.ttimer = ((self.BufferSize/FsScope)-0.05)*1000
-        
-        for Row, pars in RowsConfig.items():
-            self.RowsList.append(str(Row))
-            self.LSB = np.append(self.LSB, RowsConfig[str(Row)]['AcqVRange']/(2**16))
-            
-        for s in self.RowsList:
-            self.Channels.append(int(re.split(r'(\d+)', s)[1])-1)
-        Sig = {}
-        for col, pars in ColsConfig.items():
-            PropSig = {}
-            for p, val in pars.items():
-                if p == 'ResourceScope' or p == 'Index':
-                    continue
-                PropSig[str(p)] = val
-                
-            Sig[str(col)]= PropSig
-        print(Sig)   
-        self.Cols.Gen_SetSignal(SigsPars=Sig, 
-                                Vcm=CMVoltage)
-        self.Timer = Qt.QTimer()
-        self.Timer.moveToThread(self)
-        
+        self.DaqInterface = CoreMod.ChannelsConfig(**ChannelsConfigKW)
+        self.DaqInterface.DataEveryNEvent = self.NewData
+        self.SampKw = SampKw
+        self.AvgIndex = AvgIndex
+
     def run(self, *args, **kwargs):
-        print('start ')      
-        self.OutData = np.ndarray((self.BufferSize, len(self.Channels)))
-        self.BinData = np.ndarray((self.BufferSize, len(self.Channels)))
-        self.IntData = np.ndarray((self.BufferSize, len(self.Channels)))
-        self.initSessions()
+        self.DaqInterface.StartAcquisition(**self.SampKw)
         loop = Qt.QEventLoop()
         loop.exec_()
-    
-    def GenData(self):
-        if not self.isRunning():
-            return
-        try:
-            Inputs = self.Rows.SesScope.channels[self.Channels].fetch(num_samples=self.BufferSize,
-                                                                      relative_to=niscope.FetchRelativeTo.READ_POINTER,
-                                                                      offset=self.OffsetRows,
-                                                                      record_number=0,
-                                                                      num_records=1,
-                                                                      timeout=2)
-            
-            self.Timer.singleShot(self.ttimer, self.GenData)
-            for i, In in enumerate(Inputs):
-                self.OutData[:, i] = np.array(In.samples)/self.GainBoard 
-#                self.BinData[:,i] = np.array(In.samples)[:,i]/self.LSB[i]
-                self.BinData[:,i] = np.array(In.samples)/self.LSB[i]
-                self.IntData[:,i] = np.int16(np.round(self.BinData[:,i]))
-            self.NewData.emit()
 
-        except Exception:
-            print(Exception.args)
-            print('Requested data has been overwritten in memory')
-            self.stopSessions()
-            print('Gen and Scope Sessions Restarted')
-            self.initSessions()        
-                 
-    def initSessions(self):
-        self.Cols.Session_Gen_Initiate()
-        self.Rows.Session_Scope_Initiate()
-        self.Timer.singleShot(self.ttimer, self.GenData)
-        
-    def stopSessions(self):
-        self.Cols.Session_Gen_Abort()
-        self.Rows.Session_Scope_Abort()
-        
-    def stopTimer(self):
-        self.Timer.stop()
-        self.Timer.killTimer(self.Id)
-        
-##############################NoThread##########################################         
-class DataAcquisition():    
-    def __init__(self, ColsConfig, FsGen, GenSize, RowsConfig, FsScope, GainBoard, ResourceScope):
-        self.Columns = NifGen.Columns(ColsConfig, FsGen, GenSize)
-        self.Rows = NiScope.Rows(RowsConfig, FsScope, ResourceScope)
-        self.GainBoard = GainBoard
-        self.LSB = np.array([])
-        self.RowsList = []
-        for Row, pars in RowsConfig.items():
-            self.RowsList.append(str(Row))
-            self.LSB = np.append(self.LSB, RowsConfig[str(Row)]['AcqVRange']/(2**16))
-        
-        self.Channels=[]
-        for s in self.RowsList:
-            self.Channels.append(int(re.split(r'(\d+)', s)[1])-1)
-       
-        
-    def setSignals(self, ColsConfig, Vcm):
-        Sig = {}
-        for col, pars in ColsConfig.items():
-            PropSig = {}
-            for p, val in pars.items():
-                if p == 'ResourceScope' or p == 'Index':
-                    continue
-                PropSig[str(p)] = val
-                
-            Sig[str(col)]= PropSig
+    def CalcAverage(self, MuxData):
+        return np.mean(MuxData[:, self.AvgIndex:, :], axis=1)
 
-        self.Columns.Gen_SetSignal(Sig, Vcm)  
-        
-    def GetData(self, BufferSize, channels, OffsetRows, dtype):                
-        Inputs = self.Rows.SesScope.channels[channels].fetch(num_samples=BufferSize,
-                                                              relative_to=niscope.FetchRelativeTo.READ_POINTER,
-                                                              offset=OffsetRows,
-                                                              record_number=0,
-                                                              num_records=1,
-                                                              timeout=60)
-        OutData = np.ndarray((BufferSize, len(channels))) 
-        BinData = np.ndarray((BufferSize, len(channels)))
-        IntData = np.ndarray((BufferSize, len(channels)), dtype='int16')
-        for i, In in enumerate(Inputs):
-            OutData[:, i] = np.array(In.samples)#/self.GainBoard 
-            BinData[:,i] = OutData[:,i]/self.LSB[i]
-            IntData[:,i] = np.int16(np.round(BinData[:,i]))
-        if dtype == 'int16':
-            Output = IntData
-        else:
-            Output = OutData
-        return Output, self.LSB
-        
-    def initSessions(self):
-        self.Columns.Session_Gen_Initiate()
-        self.Rows.Session_Scope_Initiate()
-        
-    def stopSessions(self):
-        self.Columns.Session_Gen_Abort()
-        self.Rows.Session_Scope_Abort()
+    def NewData(self, aiData, MuxData):
+        self.OutData = self.CalcAverage(MuxData)
+        self.aiData = aiData
+        self.NewMuxData.emit()
---
+++ Pyxi/FMAcqCore.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 18 12:20:11 2019
+
+@author: Lucia
+"""
+
+import DaqInterface as DaqInt
+#import PyTMCore.DaqInterface as DaqInt
+import numpy as np
+
+# Daq card connections mapping 'Chname':(AI+, AI-)
+aiChannels = {'Ch01': ('ai0', 'ai8'),
+              'Ch02': ('ai1', 'ai9'),
+              'Ch03': ('ai2', 'ai10'),
+              'Ch04': ('ai3', 'ai11'),
+              'Ch05': ('ai4', 'ai12'),
+              'Ch06': ('ai5', 'ai13'),
+              'Ch07': ('ai6', 'ai14'),
+              'Ch08': ('ai7', 'ai15'),
+              'Ch09': ('ai16', 'ai24'),
+              'Ch10': ('ai17', 'ai25'),
+              'Ch11': ('ai18', 'ai26'),
+              'Ch12': ('ai19', 'ai27'),
+              'Ch13': ('ai20', 'ai28'),
+              'Ch14': ('ai21', 'ai29'),
+              'Ch15': ('ai22', 'ai30'),
+              'Ch16': ('ai23', 'ai31'),
+              }
+
+##############################################################################
+
+
+class ChannelsConfig():
+
+    # DCChannelIndex[ch] = (index, sortindex)
+    DCChannelIndex = None
+    ACChannelIndex = None
+    ChNamesList = None
+    AnalogInputs = None
+    DigitalOutputs = None
+
+    # Events list
+    DataEveryNEvent = None
+    DataDoneEvent = None
+    
+    def __init__(self, Channels, AcqSingle=True, AcqDiff=False):
+        print('InitChannels')
+
+        self.ChNamesList = sorted(Channels)
+        print(self.ChNamesList)
+        self.AcqS = AcqSingle
+        self.AcqD = AcqDiff
+        self._InitAnalogInputs()
+
+    def _InitAnalogInputs(self):
+        print('InitAnalogInputs')
+        self.SChannelIndex = {}
+        self.DChannelIndex = {}
+        InChans = []
+        index = 0
+        sortindex = 0
+        for ch in self.ChNamesList:
+#            if self.AcqS:
+            #Output+ is always read
+            InChans.append(aiChannels[ch][0]) #only Output+
+            self.SChannelIndex[ch] = (index, sortindex)
+            index += 1
+            print(ch, ' Single -->', aiChannels[ch][0])
+            print('SortIndex ->', self.DCChannelIndex[ch])
+            if self.AcqD:
+            #Only read Output- when diff activated
+                InChans.append(aiChannels[ch][1])
+                self.DChannelIndex[ch] = (index, sortindex)
+                index += 1
+                print(ch, ' AC -->', aiChannels[ch][1])
+                print('SortIndex ->', self.ACChannelIndex[ch])
+            sortindex += 1
+        print('Input ai', InChans)
+
+        self.AnalogInputs = DaqInt.ReadAnalog(InChans=InChans)
+        # events linking
+        self.AnalogInputs.EveryNEvent = self.EveryNEventCallBack
+        self.AnalogInputs.DoneEvent = self.DoneEventCallBack
+        
+
+    def StartAcquisition(self, Fs, nSampsCo, nBlocks, Vgs, Vds, **kwargs):
+        print('StartAcquisition')
+        self.SetBias(Vgs=Vgs, Vds=Vds)
+        self.SetDigitalOutputs(nSampsCo=nSampsCo)
+        print('DSig set')
+        self.nBlocks = nBlocks
+        self.nSampsCo = nSampsCo
+#        self.OutputShape = (nColumns * nRows, nSampsCh, nblocs)
+        self.OutputShape = (len(self.MuxChannelNames), nSampsCo, nBlocks)
+        EveryN = len(self.DigColumns)*nSampsCo*nBlocks
+        self.AnalogInputs.ReadContData(Fs=Fs,
+                                       EverySamps=EveryN)
+
+
+    def EveryNEventCallBack(self, Data):
+        print('EveryNEventCallBack')
+        _DataEveryNEvent = self.DataEveryNEvent
+
+        if _DataEveryNEvent is not None:
+            if self.AcqS:
+                _DataEveryNEvent = Data
+#            if self.AcqD:
+#                self.AiOutput
+
+
+    def DoneEventCallBack(self, Data):
+        print('Done callback')
+
+    def Stop(self):
+        print('Stopppp')
+        self.AnalogInputs.StopContData()
