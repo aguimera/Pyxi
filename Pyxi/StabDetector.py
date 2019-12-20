c13513ab70dca53b372804e9ebd5a7e6cbf249bf
luciare <lucia.re@imb-cnm.csic.es>
2019-12-20 12:18:10 +0100

    warnings

--- Pyxi/StabDetector.py
+++ Pyxi/StabDetector.py
@@ -11,135 +11,141 @@ from scipy.stats import linregress as lnr
 import Pyxi.CalcPSD as PSD
 import PyqtTools.SaveDictsModule as SaveDicts
 
+
 class StbDetThread(Qt.QThread):
     NextVg = Qt.pyqtSignal()
-    def __init__(self, VdVals, VgVals, MaxSlope, TimeOut, nChannels, ChnName, PlotterDemodKwargs):
-       '''Initialization for Stabilitation Detection Thread
-          VdVals: Array. Contains the values to use in the Vd Sweep.
+
+    def __init__(self, VdVals, VgVals, MaxSlope, TimeOut, nChannels,
+                 ChnName, PlotterDemodKwargs):
+        '''Initialization for Stabilitation Detection Thread
+           VdVals: Array. Contains the values to use in the Vd Sweep.
                           [0.1, 0.2]
-          VgVals: Array. Contains the values to use in the Vg Sweep
+           VgVals: Array. Contains the values to use in the Vg Sweep
                           [0., -0.1, -0.2, -0.3]
-          MaxSlope: float. Specifies the maximum slope permited to consider the system is
-                           stabilazed, so the data is correct to be acquired
-          TimeOut: float. Specifies the maximum amount of time to wait the signal
-                          to achieve MaxSlope, if TimeOut is reached the data is 
-                          save even it is not stabilazed
-          nChannels: int. Number of acquisition channels (rows) active
-          ChnName: dictionary. Specifies the name of Row+Column beign processed 
-                               and its index:
-                               {'Ch04Col1': 0, 
-                               'Ch05Col1': 1, 
-                               'Ch06Col1': 2}
-          PlotterDemodKwargs: dictionary. Contains Demodulation Configuration an parameters
-                                          {'Fs': 5000.0, 
-                                          'nFFT': 8.0, 
-                                          'scaling': 'density', 
-                                          'nAvg': 50}
-       '''
-       super(StbDetThread, self).__init__() 
-       self.ToStabData = None
-       self.Stable = False
-       self.Datos = None
-       
-       self.MaxSlope = MaxSlope
-       self.TimeOut = TimeOut
-       
-       self.VgIndex=0
-       self.VdIndex=0
-       
-       self.Timer = Qt.QTimer()
-       self.Timer.moveToThread(self)
-       print('nchannels',nChannels)
-       self.threadCalcPSD = PSD.CalcPSD(nChannels=nChannels,
-                                        **PlotterDemodKwargs)
-       self.threadCalcPSD.PSDDone.connect(self.on_PSDDone)
-       self.SaveDCAC = SaveDicts.SaveDicts(SwVdsVals=VdVals,
-                                           SwVgsVals=VgVals,
-                                           Channels=ChnName,
-                                           nFFT=int(PlotterDemodKwargs['nFFT']),
-                                           FsDemod=PlotterDemodKwargs['Fs']
-                                           )
-       self.SaveDCAC.PSDSaved.connect(self.on_NextVg)
-       
+           MaxSlope: float. Specifies the maximum slope permited to consider
+                            the system is stabilazed, so the data is correct
+                            to be acquired
+           TimeOut: float. Specifies the maximum amount of time to wait the
+                           signal to achieve MaxSlope, if TimeOut is reached
+                           the data is save even it is not stabilazed
+           nChannels: int. Number of acquisition channels (rows) active
+           ChnName: dictionary. Specifies the name of Row+Column beign
+                                processed and its index:
+                                {'Ch04Col1': 0,
+                                'Ch05Col1': 1,
+                                'Ch06Col1': 2}
+           PlotterDemodKwargs: dictionary. Contains Demodulation Configuration
+                                           an parameters
+                                           {'Fs': 5000.0,
+                                           'nFFT': 8.0,
+                                           'scaling': 'density',
+                                           'nAvg': 50 }
+        '''
+        super(StbDetThread, self).__init__()
+        self.ToStabData = None
+        self.Stable = False
+        self.Datos = None
+
+        self.MaxSlope = MaxSlope
+        self.TimeOut = TimeOut
+
+        self.VgIndex = 0
+        self.VdIndex = 0
+
+        self.Timer = Qt.QTimer()
+        self.Timer.moveToThread(self)
+        print('nchannels', nChannels)
+        self.threadCalcPSD = PSD.CalcPSD(nChannels=nChannels,
+                                         **PlotterDemodKwargs)
+        self.threadCalcPSD.PSDDone.connect(self.on_PSDDone)
+        self.SaveDCAC = SaveDicts.SaveDicts(SwVdsVals=VdVals,
+                                            SwVgsVals=VgVals,
+                                            Channels=ChnName,
+                                            nFFT=int(PlotterDemodKwargs['nFFT']),
+                                            FsDemod=PlotterDemodKwargs['Fs']
+                                            )
+        self.SaveDCAC.PSDSaved.connect(self.on_NextVg)
+
     def initTimer(self):
         self.Timer.singleShot((self.TimeOut*1000), self.DCIdCalc)
         self.Id = self.Timer.timerId()
-    def run(self):       
+
+    def run(self):
         while True:
             if self.ToStabData is not None:
-                
+
                 Data = np.abs(self.ToStabData[:,0])#se mira la estabilización en la priemra row adquirida
                 x = np.arange(Data.size)
                 self.ptrend = np.polyfit(x, Data, 1)
                 trend = np.polyval(self.ptrend, x)
-                
-                slope = lnr(x, trend)[0] 
+
+                slope = lnr(x, trend)[0]
                 print('ESTA ES LA PENDIENTE', slope)
                 if slope <= self.MaxSlope:
 
                     self.DCIdCalc()
-                    
+
                 self.ToStabData = None
-                                            
+
             else:
                 Qt.QThread.msleep(10)
 
     def AddData(self, NewData):
         if self.ToStabData is not None:
             print('Error STAB!!!!')
-            
+
         if self.Stable is False:
-            self.ToStabData = NewData 
+            self.ToStabData = NewData
             print('SaveDatos')
-            self.Datos = self.ToStabData#se guardan los datos para que no se sobreescriban
+            self.Datos = self.ToStabData  # se guardan los datos para que no se sobreescriban
         if self.Stable is True:
             self.threadCalcPSD.AddData(NewData)
-    
+
     def DCIdCalc(self):
         print('DATA STAB')
-        #se descoencta el timer 
+        # se descoencta el timer
         self.Timer.stop()
         self.Timer.killTimer(self.Id)
-        
+
         self.ToStabData = None
-        #se activa el flag de estable
+        # se activa el flag de estable
         self.Stable = True
-        #se activa el thread para calcular PSD
+        # se activa el thread para calcular PSD
         self.threadCalcPSD.start()
-        #se obtiene el punto para cada Row
+        # se obtiene el punto para cada Row
         DCIds = np.ndarray((self.Datos.shape[1], 1))
         for ind in range(self.Datos.shape[1]):
-            Data = np.abs(self.Datos[:,ind])
+            Data = np.abs(self.Datos[:, ind])
             x = np.arange(Data.size)
             self.ptrend = np.polyfit(x, Data, 1)
-                        
-            DCIds[ind] = (2*self.ptrend[-1])/np.sqrt(2) #Se toma el ultimo valor
-        
-        #Se guardan los valores DC
-        self.SaveDCAC.SaveDCDict(Ids=DCIds, 
-                                 SwVgsInd=self.VgIndex, 
+
+            DCIds[ind] = (2*self.ptrend[-1])/np.sqrt(2)  # Se toma el ultimo valor
+
+        # Se guardan los valores DC
+        self.SaveDCAC.SaveDCDict(Ids=DCIds,
+                                 SwVgsInd=self.VgIndex,
                                  SwVdsInd=self.VdIndex)
-        
+
     def on_PSDDone(self):
         print('PSD DONE RECIBED')
         self.freqs = self.threadCalcPSD.ff
         self.PSDdata = self.threadCalcPSD.psd
-        #se desactiva el thread para calcular PSD
+        # se desactiva el thread para calcular PSD
         self.threadCalcPSD.stop()
-        #Se guarda en AC dicts
+        # Se guarda en AC dicts
         self.SaveDCAC.SaveACDict(psd=self.PSDdata,
                                  ff=self.freqs,
-                                 SwVgsInd=self.VgIndex, 
+                                 SwVgsInd=self.VgIndex,
                                  SwVdsInd=self.VdIndex
                                  )
-            
+
     def on_NextVg(self):
-        #Y se emite la señal para el siguiente sweep de VG
+        # Y se emite la señal para el siguiente sweep de VG
         print('NEXTVG EMIT')
-        self.NextVg.emit() 
-        
+        self.NextVg.emit()
+
     def stop(self):
         self.threadCalcPSD.PSDDone.disconnect()
         self.SaveDCAC.PSDSaved.disconnect()
         self.threadCalcPSD.stop()
-        self.terminate()      
+        self.terminate()
